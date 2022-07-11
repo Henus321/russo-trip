f3031f8a669868d8693ae834936804538e2b1faf
Henus321 <tyrant321@yandex.ru>
2022-07-11 22:03:12 +0300

    navLink added to navigation, footer social media links added and re-styled

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,5 +1,9 @@
 import Button from '../button/button.component';
 
+import { FaVk } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
+
 import './footer.styles.scss';
 
 const Footer = () => {
@@ -8,16 +12,43 @@ const Footer = () => {
       <div className="footer__row">
         <div className="footer__column">
           <h2 className="footer__title">About</h2>
-          <ul className="footer__nav-list">
-            <li className="footer__nav-item">FAQ</li>
-            <li className="footer__nav-item">Home</li>
-            <li className="footer__nav-item">About Us</li>
-            <li className="footer__nav-item">Contacts</li>
-            <li className="footer__nav-item">Registration</li>
-          </ul>
-          <div className="footer__social-media"></div>
+          <nav className="footer__nav">
+            <ul className="footer__nav-list">
+              <li className="footer__nav-item">FAQ</li>
+              <li className="footer__nav-item">Home</li>
+              <li className="footer__nav-item">About Us</li>
+              <li className="footer__nav-item">Contacts</li>
+              <li className="footer__nav-item">Registration</li>
+            </ul>
+          </nav>
+          <div className="footer__logo-container">
+            <a
+              className="footer__logo"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/Henus321"
+            >
+              <FaGithub />
+            </a>
+            <a
+              className="footer__logo"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://vk.com/"
+            >
+              <FaVk />
+            </a>
+            <a
+              className="footer__logo"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://youtube.com/"
+            >
+              <FaYoutube />
+            </a>
+          </div>
         </div>
-        <div className="footer__column">
+        <div className="footer__column footer__column-middle">
           <h2 className="footer__title">Feedback</h2>
           <form className="footer__form" action="">
             <textarea
@@ -28,28 +59,27 @@ const Footer = () => {
               rows="8"
             ></textarea>
             {/* Add handler later */}
-            <Button buttonType="btn__black-submit" buttonText={'Submit'} />
+            <Button
+              buttonType="btn__black-submit"
+              buttonText="Submit&nbsp;&rarr; "
+            />
           </form>
         </div>
         <div className="footer__column">
-          <h2 className="footer__title">Some Text</h2>
+          <h2 className="footer__title">Permissions</h2>
           <p className="footer__paragraph">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
-            voluptates, pariatur, earum id temporibus dolores nostrum maiores
-            modi doloremque natus neque cumque, facere possimus aliquid. Nam
-            cupiditate animi autem aliquam!
+            The activity is carried out in accordance with the regulations on
+            licensing of tour operator and travel agency activities approved by
+            the Decree of the Government of the Russian Federation No. 95 dated
+            11.02.2022 and Federal Law No. 128-FZ dated 08.08.2001 "On Licensing
+            of certain types of activities".
           </p>
         </div>
       </div>
       <div className="footer__hr"></div>
       <div className="footer__row">
-        <p className="footer__paragraph footer__rights">Privacy Policy</p>
-        <p className="footer__paragraph footer__rights">
-          2022 @ all rights not reserved
-        </p>
-        <p className="footer__paragraph footer__rights">
-          Developed by Russo Trip Studio
-        </p>
+        <p className="footer__rights">2022 @ all rights not reserved</p>
+        <p className="footer__rights">Developed by Russo Trip</p>
       </div>
     </footer>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/user.context';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { onLogout } from '../../utils/firebase/firebase.utils';
 
@@ -13,6 +13,9 @@ const Header = () => {
   const navigate = useNavigate();
   // const location = useLocation(); // later to know active
   const { currentUser } = useContext(UserContext);
+
+  const setNavLinkActive = ({ isActive }) =>
+    isActive ? 'header__link-active' : 'header__link';
 
   // TEMPORARY
   const logout = () => {
@@ -32,32 +35,34 @@ const Header = () => {
       </div> */}
       <nav>
         <ul className="header__nav-list">
-          <li className="header__nav-item" onClick={() => navigate('/')}>
-            <span className="header__link">Home</span>
+          <li className="header__nav-item">
+            <NavLink to={'/'} className={setNavLinkActive}>
+              Home
+            </NavLink>
           </li>
-          <li className="header__nav-item" onClick={() => navigate('/about')}>
-            <span className="header__link">About Us</span>
+          <li className="header__nav-item">
+            <NavLink to="/about" className={setNavLinkActive}>
+              About
+            </NavLink>
           </li>
-          <li
-            className="header__nav-item"
-            onClick={() => navigate('/contacts')}
-          >
-            <span className="header__link">Contacts</span>
+          <li className="header__nav-item">
+            <NavLink to="/contacts" className={setNavLinkActive}>
+              Contacts
+            </NavLink>
           </li>
           {currentUser ? (
             <>
-              <li
-                className="header__nav-item"
-                onClick={() => navigate('/create-pathway')}
-              >
-                <span>Create Pathway</span>
+              <li className="header__nav-item">
+                <NavLink to="/create-pathway" className={setNavLinkActive}>
+                  Create Pathway
+                </NavLink>
               </li>
               <li className="header__nav-item">
                 <span className="header__link">
                   <button
                     type="button"
                     onClick={logout}
-                    className="header__logout-btn"
+                    className="header__link header__logout-btn"
                   >
                     Log Out
                   </button>
@@ -65,7 +70,7 @@ const Header = () => {
               </li>
               <li className="header__nav-item header__log-in">
                 <img
-                  className="header__avatar"
+                  className="header__link header__avatar"
                   src={
                     currentUser &&
                     (currentUser.photoURL
@@ -78,17 +83,15 @@ const Header = () => {
             </>
           ) : (
             <>
-              <li
-                className="header__nav-item"
-                onClick={() => navigate('/registration')}
-              >
-                <span className="header__link">Registration</span>
+              <li className="header__nav-item">
+                <NavLink to="/registration" className={setNavLinkActive}>
+                  Registration
+                </NavLink>
               </li>
-              <li
-                className="header__nav-item"
-                onClick={() => navigate('/sign-in')}
-              >
-                <span className="header__link">Sign In</span>
+              <li className="header__nav-item">
+                <NavLink to="/sign-in" className={setNavLinkActive}>
+                  Sign In
+                </NavLink>
               </li>
             </>
           )}
