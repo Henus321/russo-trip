e75afc4e4266b465a1df4ee6994608c04c5c521f
Henus321 <tyrant321@yandex.ru>
2022-07-14 17:42:45 +0300

    swiper slider photos refreshed

--- src/components/slider/slider.component.jsx
+++ src/components/slider/slider.component.jsx
@@ -10,7 +10,11 @@ import './slider.styles.scss';
 const data = [
   {
     imageUrl:
-      'https://images.unsplash.com/photo-1567449303070-ed66e1d59843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      'https://images.unsplash.com/photo-1602839424531-b73a03c15453?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1535383749833-770a4e09edec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
   {
     imageUrl:
@@ -19,6 +23,10 @@ const data = [
   {
     imageUrl:
       'https://images.unsplash.com/photo-1592150587525-90486c792197?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1564241234953-1075d07f1c9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
 ];
 
