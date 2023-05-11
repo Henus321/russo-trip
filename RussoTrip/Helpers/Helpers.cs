9f6e32dd45bfe21a6b0cd13e09438c064bbf0988
Александр Ерхов <henus321@MacBook-Air-Aleksandr.local>
2023-05-11 17:09:54 +0300

    feat(layout): Added cities localization helper

---
+++ RussoTrip/Helpers/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace RussoTrip.Helpers
+{
+	public static class Helpers
+	{
+        public static string GetLocaleCity(string city)
+        {
+            switch (city.ToLower())
+            {
+                case "moscow":
+                    return "Москва";
+                case "saint-petersburg":
+                    return "Санкт-Петербург";
+                case "kazan":
+                    return "Казань";
+                case "vladivostok":
+                    return "Владивосток";
+                default:
+                    return "";
+            }
+        }
+    }
+}
+
